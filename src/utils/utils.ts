e14ff2fd6d159727bbf8ac194e24caa9fad52f2d
liam-iv <backgroundpositioncenter@gmail.com>
2023-12-09 20:38:17 +0300

    [hacker-news] - внес правки по стилям

---
+++ src/utils/utils.ts
@@ -0,0 +1,1 @@
+export function unixToDate (unixTime: number) {  const date = new Date(unixTime * 1000)  const dd = date.getDate()  const mm = date.getMonth() + 1  const yy = date.getFullYear()  return `${dd}-${mm}-${yy}`}
